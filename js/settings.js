1aef12c60f6ecbf93fc272661fd6da3aae525620
jrvalza <jvalencia658@gmail.com>
2024-05-18 19:43:20 +0200

    https

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,3 @@
-export var URL_API = "http://37.187.181.98/juavaal2-api"
+export var URL_API = "https://37.187.181.98/juavaal2-api"
 //export var URL_API="http://localhost:8000"
 //export var URL_GEOSERVER="http://localhost:8080/geoserver"
